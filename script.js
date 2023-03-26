44fea3af649b21ef8e1a9affa97217c66a525d6b
Say-endo <128940496+Say-endo@users.noreply.github.com>
2023-03-26 02:31:11 -0300

    change way to get text

--- script.js
+++ script.js
@@ -22,9 +22,10 @@
     // Alternate method: Use AJAX to load the contents of an external file into a div based on URL fragment
     // This will extract the region name from URL hash, and then load [region].html into the main #content div
     var region = location.hash.toString() || '#first';
-    console.log(region.slice(1));
-    var uno = $.get(region.slice(1) + '.txt');
-    document.getElementById("texto").innerText = uno.responseText
+    var texto = $.get(region.slice(1) + '.txt');
+    if (texto){      
+      document.getElementById("texto").innerText = text.responseText;
+    }
     
   });
   
